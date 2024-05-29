24ee6677c9afcb5866f7be9cdf211e7ce7bbfbcc
张鸿运 <1836017030@qq.com>
2024-05-29 13:28:27 +0800

    增加示例代码

--- main.go
+++ main.go
@@ -4,22 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 	"html/template"
 	"net/http"
-	"os"
 )
+
+func add(x, y int) int {
+	return x + y
+}
 
 func main() {
 	r := gin.New()
+	r.SetFuncMap(template.FuncMap{
+		"add": add,
+	})
 	r.LoadHTMLGlob("template/*")
 	r.GET("/grammar", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "grammar", gin.H{
 			"hello": "Hello World",
+			"map":   map[string]string{"key1": "value1", "key2": "value2"},
+			"slice": []string{"slice1", "slice2", "slice3"},
 		})
 	})
-	parse, err := template.New("").Parse("")
-	if err != nil {
-		return
-	}
-	parse.Execute(os.Stdout, "")
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8888")
 }
